e3a1e3cbee3f6085fc99adfead1b01477619a7a1
Rizwan-Necun <rizwanm@necun.in>
2024-03-02 12:44:13 +0530

    All changes

---
+++ methods/customExceptions.py
@@ -0,0 +1,126 @@
+def signup_exception(e):
+    status = {
+        "User created successfully" : 201100,
+        
+        "Invalid phone number" : 400100,
+        "Invalid Email format" : 400101,
+        "Missing fields" : 400102,
+        
+        
+        "Username already exists" : 409100,
+        "Email already exists" : 409101,
+        "Phone Number already exists" : 409102,
+        "Duplicate entry for unique field":409103,
+        'Missing fields':400103,
+        'Headers Invalid':409104,
+        "Required headers not found":400105,
+        'Full Name must contain only Alphabets':400106,
+        'password must contain greater than 7 letters': 400107,
+        
+        
+        'Database connection error ': 500100,
+        'Data Integrity Violation' : 500101,
+        'Resource Exhaustion' : 500102,
+    }
+    
+    return status[e]
+    
+    
+
+def signin_exception(e):
+    status = {
+        "Login successful" : 200200,
+        "Invalid username or password" : 400201,
+        "Username is  missing":400202,
+        "Password is missing" : 400203,
+        "User not found" : 400204,
+        "Email already exists":400205,
+        
+        
+        'Database connection error ': 500200,
+        'Data Integrity Violation' : 500201,
+        'Resource Exhaustion' : 500202,
+        
+    }
+    
+    return status[e]
+        
+        
+    
+    
+    
+def forgot_password_exception(e):
+    status = {
+        "Invalid Email format" : 400301,
+        "Invalid Email" : 400302,
+        "Email missing" : 400303,
+        'Email is required' : 400304,
+        
+        'Database connection error ': 500300,
+        'Data Integrity Violation' : 500301,
+        'Resource Exhaustion' : 500302,
+    }
+    
+    return status[e]
+    
+    
+def reset_password_exception(e):
+    status = {
+        "Missing or Invalid Password" : 400401,
+        "Invalid or expired token" : 400402,
+        "Passwords did not match" : 400403,
+        "password has been upodated successfully" : 200400,
+        'Email is required' : 401404,
+        
+        "password reset link has been sent to your mail": 200405,
+        
+        'Database connection error ': 500400,
+        'Data Integrity Violation' : 500401,
+        'Resource Exhaustion' : 500402,
+    }
+    
+    return status[e]
+
+def image_upload_exception(e):
+    status = {
+        "Image uploaded successfully" : 200500,
+        "Failed to upload image" : 500501,
+        
+        'Database connection error ': 500500,
+        'Data Integrity Violation' : 500501,
+        'Resource Exhaustion' : 500502,
+    }
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    # 409 Conflict
+    # 201 created
+    # 400 Bad Request
+    # 401 Unauthorized
+    # 403 Forbidden
+    # 404 Not Found
+    # 405 Method Not Allowed
+    # 406 Not Acceptable
+    # 407 Proxy Authentication Required
+    # 408 Request Timeout
+    # 409 Conflict
+    # 410 Gone
+    # 411 Length Required
+    # 412 Precondition Failed
+    # 413 Request Entity Too Large
+    # 414 Request-URI Too Long
+    # 415 Unsupported Media Type
+    # 416 Requested Range Not Satisfiable
+    # 417 Expectation Failed
+    # 418 I'm a teapot
+    # 422 Unprocessable Entity
+    # 423 Locked
+    # 424 Failed Dependency
+    # 425 Unordered Collection
--- methods/method.py
+++ methods/method.py
@@ -1,11 +1,13 @@
 from flask import request, jsonify
 from werkzeug.security import generate_password_hash
-import random  
-from database_renote.operations import db_methods   
+import random
+from database_renote.operations import db_methods
 from utils import redis_config
 from azure.storage.blob import BlobServiceClient
 from werkzeug.utils import secure_filename
 import re
+from datetime import datetime
+from methods.customExceptions import forgot_password_exception,reset_password_exception,signin_exception,signup_exception
 
 utils_instance=redis_config()
 
@@ -21,134 +23,332 @@ CONTAINER_NAME = 'pictures'
 class all_methods:
     def hello(self):
         print("hellloo000000000 world")
-        
-    def allowed_file(filename):
+    def allowed_file(self,filename):
         return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
-        
     def generate_unique_user_id(self):
         return random.randint(10**15, (10**16)-1)
-    def validate_email(email):
+    def validate_email(self,email):
         email_pattern= r'^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$'
         return re.match(email_pattern,email)
-    
-    def validate_phonenumber(phone_number):
+    def validate_phonenumber(self,phone_number):
         phone_number_pattern= r'^\d{10}$'
         return re.match(phone_number_pattern,phone_number)
-    
-    def validate_fullname(fullname):
-        fullname_pattern=r'^[a-zA-Z]$'
+    def validate_fullname(self,fullname):
+        fullname_pattern=r'^[a-z A-Z]+$'
         return re.match(fullname_pattern,fullname)
-    
+    def password_strength_validation(self,password):
+            return len(password) > 7
     def signup(self):
         data = request.json
         print("Headers Received:", request.headers)
         required_fields = ['fullname', 'username', 'password', 'email', 'phone_number']
         missing_fields = [field for field in required_fields if field not in data or not data[field]]
-    
+
         if missing_fields:
-            return jsonify({'message': 'Missing fields', 'missing': missing_fields}), 400
-    
+            error_response = {
+                "error": {
+                    "status": "400",
+                    "message": "Missing fields",
+                    "messageKey": "missing-fields-txt",
+                    "details": f"The following fields are missing: {missing_fields}",
+                    "type": "ValidationException",
+                    "code": 400103,
+                    "timeStamp": datetime.utcnow().strftime('%Y-%m-%d %H:%M:%S +0000'),
+                    "instance": "/v1/"
+                }
+            }
+            return jsonify(error_response), 400
+
+
         if 'Application' in request.headers and 'Clientid' in request.headers:
             application_id = request.headers['Application']
             client_id = request.headers['Clientid']
+            if application_id != 'renote' or client_id != 'necun':
+                error_response = {
+                        "error": {
+                        "status": "400",
+                        "message": "Headers Invalid",
+                        "messageKey": "invalid-headers-txt",
+                        "details": "One or more of the request headers are invalid or missing.",
+                        "type": "HeaderValidationException",
+                        "code": 400104,
+                        "timeStamp": datetime.utcnow().strftime('%Y-%m-%d %H:%M:%S +0000'),
+                        "instance": "/v1/"  # Optional, include if relevant to your application
+                    }
+                }
+                return jsonify(error_response), 400
         else:
-            print("Required headers not found")
-    
-        user_id=self.generate_unique_user_id()      
+            error_response = {
+                "error": {
+                    "status": "400",
+                    "message": "Required headers not found",
+                    "messageKey": "required-headers-missing-txt",
+                    "details": "The request is missing one or more required headers.",
+                    "type": "HeaderValidationException",
+                    "code": 400105,
+                    "timeStamp": datetime.utcnow().strftime('%Y-%m-%d %H:%M:%S +0000'),
+                    "instance": "/v1/"  # Optional, include if relevant to your application
+                }
+            }
+            return jsonify(error_response), 400
+
+        user_id=self.generate_unique_user_id()
         fullname = data['fullname']
         username = data['username']
         password = generate_password_hash(data['password'])
         email = data['email']
-        phone_number = data['phone_number'] 
+        phone_number = data['phone_number']
         profile_pic=' '
-        
+
         if not self.validate_email(email):
-            return jsonify({'message':'Invalid Email format'}), 400
-        
+            error_response = {
+                "error": {
+                    "status": "400",
+                    "message": "Invalid Email format",
+                    "messageKey": "invalid-email-format-txt",
+                    "details": "The email address provided does not match the expected format.",
+                    "type": "ValidationException",
+                    "code": 400101,
+                    "timeStamp": datetime.utcnow().strftime('%Y-%m-%d %H:%M:%S +0000'),
+                    "instance": "/v1/"  # Optional, include if relevant to your application
+                }
+            }
+            return jsonify(error_response), 400
+
+
         if not self.validate_phonenumber(phone_number):
-            return jsonify ({'message':'Invalid phone number'}), 400
-        
+            error_response = {
+                "error": {
+                    "status": "400",
+                    "message": "Invalid phone number",
+                    "messageKey": "invalid-phone-number-txt",
+                    "details": "The phone number provided does not match the expected format.",
+                    "type": "ValidationException",
+                    "code": 400100,
+                    "timeStamp": datetime.utcnow().strftime('%Y-%m-%d %H:%M:%S +0000'),
+                    "instance": "/v1/"  # Optional, include if relevant to your application
+                }
+            }
+            return jsonify(error_response), 400
+
+
         if not self.validate_fullname(fullname):
-            return jsonify({'message':'Full Name must contain only Alphabets'}), 400
-            
-            
-    
+            error_response = {
+                "error": {
+                    "status": "400",
+                    "message": "Full Name must contain only Alphabets",
+                    "messageKey": "full-name-alphabets-only-txt",
+                    "details": "The full name provided contains invalid characters. It must only include alphabets.",
+                    "type": "ValidationException",
+                    "code": 400106,
+                    "timeStamp": datetime.utcnow().strftime('%Y-%m-%d %H:%M:%S +0000'),
+                    "instance": "/v1/"  # Optional, include if relevant to your application
+                }
+            }
+            return jsonify(error_response), 400
+
+
+        if not self.password_strength_validation(password):
+            error_response = {
+                "error": {
+                    "status": "400",
+                    "message": "Password must contain more than 7 letters",
+                    "messageKey": "password-length-txt",
+                    "details": "The password provided is too short. It must contain more than 7 characters.",
+                    "type": "ValidationException",
+                    "code": 400107,
+                    "timeStamp": datetime.utcnow().strftime('%Y-%m-%d %H:%M:%S +0000'),
+                    "instance": "/v1/"  # Optional, include if relevant to your application
+                }
+            }
+            return jsonify(error_response), 400
+
+
         response=db_instance.signup_db_operation(application_id, client_id, user_id, username,  password, email , fullname, phone_number , profile_pic, 0)
         if response is not None:
             return response
-        return jsonify({'message': 'User created successfully'}), 201
-    
+        success_response = {
+            "status": "201",
+            "code": "201100",
+            "message": "User created successfully",
+            "details": {
+                "messageKey": "user-created-successfully-txt",
+                "description": "The user account has been successfully created.",
+                "type": "CreationSuccess",
+                "timeStamp": datetime.utcnow().strftime('%Y-%m-%d %H:%M:%S +0000'),
+                "instance": "/v1/"  # Optional, include if relevant to your application
+            }
+        }
+        return jsonify(success_response), 201
+
+
     def signin(self,a):
         data = request.json
-   
+
         username = data['username']
         password = data['password']
-        
         if not username:
-            return jsonify({'message':'Username is  missing'}), 400
-        
+            error_response = {
+                "error": {
+                    "status": "400",
+                    "message": "Username is missing",
+                    "messageKey": "username-missing-txt",
+                    "details": "The request did not include a username, which is required.",
+                    "type": "ValidationException",
+                    "code": 400202,
+                    "timeStamp": datetime.utcnow().strftime('%Y-%m-%d %H:%M:%S +0000'),
+                    "instance": "/v1/"  # Optional, include if relevant to your application
+                }
+            }
+            return jsonify(error_response), 400
+
         if not password:
-            return jsonify({'message':'Password is missing '}), 400
-        
+            error_response = {
+                "error": {
+                    "status": "400",
+                    "message": "Password is missing",
+                    "messageKey": "password-missing-txt",
+                    "details": "The request did not include a password, which is required.",
+                    "type": "ValidationException",
+                    "code": 400203,
+                    "timeStamp": datetime.utcnow().strftime('%Y-%m-%d %H:%M:%S +0000'),
+                    "instance": "/v1/"  # Optional, include if relevant to your application
+                }
+            }
+            return jsonify(error_response), 400
+
         response=db_instance.signin_db_operation(username,password,a)
         if response is not None:
             return response
-        
     def forgot_password(self):
         email = request.json.get('email')
         if self.validate_email(email):
-            return jsonify({'message':'Email invalid'}), 400
+            error_response = {
+                "error": {
+                    "status": "400",
+                    "message": "Email invalid",
+                    "messageKey": "email-invalid-txt",
+                    "details": "The email address provided is not valid. Please provide a valid email address.",
+                    "type": "ValidationException",
+                    "code": 400302,
+                    "timeStamp": datetime.utcnow().strftime('%Y-%m-%d %H:%M:%S +0000'),
+                    "instance": "/v1/"  # Optional, include if relevant to your application
+                }
+            }
+            return jsonify(error_response), 400
+
         if not email:
-            return jsonify({'message':'Email is required'}), 400 
-        
+            error_response = {
+                "error": {
+                    "status": "400",
+                    "message": "Email is required",
+                    "messageKey": "email-required-txt",
+                    "details": "The request did not include an email address, which is required.",
+                    "type": "ValidationException",
+                    "code": 400304,
+                    "timeStamp": datetime.utcnow().strftime('%Y-%m-%d %H:%M:%S +0000'),
+                    "instance": "/v1/"  # Optional, include if relevant to your application
+                }
+            }
+            return jsonify(error_response), 400
+
+
         response=db_instance.get_user_by_email(email)
         if response is not None:
             return response
-    
+
     def reset_password(self,token):
         response=db_instance.user_by_reset_token(token)
         if response is not None:
             return response
-        
+
     def update_password(self):
         token=request.form.get('token')
         if not token:
-            return jsonify({'message':'token invalid'}), 401
+            error_response = {
+            "error": {
+                "status": "401",
+                "message": "Token missing or invalid",
+                "messageKey": "token-missing-invalid-txt",
+                "details": "The request did not include a token or included an invalid token.",
+                "type": "AuthenticationException",
+                "code": 401404,
+                "timeStamp": datetime.utcnow().strftime('%Y-%m-%d %H:%M:%S +0000'),
+                "instance": "/v1/"  # Optional, include if relevant to your application
+            }
+        }
+            return jsonify(error_response), 401
+
         new_password=request.form.get('password')
         confirm_password=request.form.get('confirm_password')
         if not new_password or confirm_password:
             if new_password != confirm_password:
-                return jsonify({'message':'passwords do not match'}), 400
-        
+                error_response = {
+                "error": {
+                    "status": "400",
+                    "message": "Passwords do not match",
+                    "messageKey": "passwords-not-match-txt",
+                    "details": "The provided passwords do not match. Please ensure both passwords are identical.",
+                    "type": "ValidationException",
+                    "code": 400403,
+                    "timeStamp": datetime.utcnow().strftime('%Y-%m-%d %H:%M:%S +0000'),
+                    "instance": "/v1/"  # Optional, include if relevant to your application
+                }
+            }
+            return jsonify(error_response), 400
+
         response=db_instance.db_method_update_password(token,new_password)
         if response is not None:
             return response
-        
+
     def upload_image(self,username):
         if 'image' not in request.files:
-            return jsonify({'message': 'No image part'}), 400
- 
+            error_response = {
+                "error": {
+                    "status": "400",
+                    "message": "No image part",
+                    "messageKey": "no-image-part-txt",
+                    "details": "The request did not include an image part. Please include an image part in the request.",
+                    "type": "ValidationException",
+                    "code": 400600,
+                    "timeStamp": datetime.utcnow().strftime('%Y-%m-%d %H:%M:%S +0000'),
+                    "instance": "/v1/upload-image"  # Optional, include if relevant to your application
+                }
+            }
+            return jsonify(error_response), 400
+
         file = request.files['image']
         if file.filename == '' or not self.allowed_file(file.filename):
-            return jsonify({'message': 'No selected file'}), 400
-    
+            error_response = {
+            "error": {
+                "status": "400",
+                "message": "No selected file",
+                "messageKey": "no-selected-file-txt",
+                "details": "No file was selected for upload. Please select a file to upload.",
+                "type": "ValidationException",
+                "code": 400601,
+                "timeStamp": datetime.utcnow().strftime('%Y-%m-%d %H:%M:%S +0000'),
+                "instance": "/v1/upload-image"  # Optional, include if relevant to your application
+            }
+        }
+            return jsonify(error_response), 400
+
+
         filename = secure_filename(file.filename)
         image_url = self.upload_to_azure_blob(file, filename)
-        
+
         response=db_instance.uploading_image_url(username , image_url)
         if response is not None:
-            return response 
-    
-        
-    
+            return response
+
     def upload_to_azure_blob(self,file_stream, file_name):
-   
+
         if not AZURE_STORAGE_CONNECTION_STRING:
             raise ValueError("The Azure Storage Connection String is not set or is empty.")
-    
+
         blob_service_client = BlobServiceClient.from_connection_string(AZURE_STORAGE_CONNECTION_STRING)
         blob_client = blob_service_client.get_blob_client(container=CONTAINER_NAME, blob=file_name)
-    
+
         blob_client.upload_blob(file_stream, overwrite=True)
-    
+
         return blob_client.url
